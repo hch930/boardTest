330c0922908e41c3423fd058b320722cb9946f72
hch <hch@DESKTOP-4KFLA75>
2021-08-09 18:06:13 +0900

    Log

---
+++ boardTest/src/main/java/com/hch/auth/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.hch.auth.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+}
